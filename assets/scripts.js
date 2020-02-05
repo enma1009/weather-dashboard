4da9304cfadc796f8f0204972a9fadad2b9e610b
enma1009 <enid.soto@gmail.com>
2020-02-05 15:55:14 -0500

    added code to retrieve the data from the API

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,20 +1,62 @@
 var APIKey = "09e0d7e534e41ce68ba5f2577fa5f760";
-var q = "Miami,US";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q=" + q + "&units=imperial&appid=" + APIKey;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-    })
-    .then(function(response) {
-    console.log(response);
-    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);
-    $(".temp").text("Temperature (F) " + response.main.temp);
+$(".search").on("click", function(){
+    
+    var q = $(".city").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&units=imperial&appid=" + APIKey;
 
-    console.log("Wind Speed: " + response.wind.speed);
-    console.log("Humidity: " + response.main.humidity);
-    console.log("Temperature (F): " + response.main.temp);
+    var forecastURL = "api.openweathermap.org/data/2.5/forecast?q="+ q;
+
+    $.ajax({ // gets the current weather info
+        url: queryURL,
+        method: "GET"
+        })
+        .then(function(response) {
+            console.log(response);
+            $("#cityMain").text(response.name);
+            $("#degreeMain").text(response.main.temp);
+            $("#humidityMain").text(response.main.humidity);
+            $("#windMain").text(response.wind.speed);
+            $("#iconMain").attr("src","http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+
+            var uvIndexcoord = "&lat="+response.coord.lat+"&lon="+response.coord.lon;
+            var cityId = response.id;
+            displayUVindex(uvIndexcoord);
+            displayForecast(cityId);
+    });
+
+   
 });
+var now = moment();
+var currentDate = now.format("dddd[,] MMMM Do gggg");
+$("#dateMain").text(currentDate);
+
+function displayUVindex(uv) {
+    //var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + k + uv;
+    
+    $.ajax({ // gets the UV index info
+        url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + uv,
+        method: "GET"
+        })
+        .then(function(response) {
+        //console.log(response);
+        $("#uvIndexMain").text(response.value);
+    });
+}
+
+function displayForecast(c) {
+    console.log(c);
+    $.ajax({ // gets the 5 day forecast
+        url: "http://api.openweathermap.org/data/2.5/forecast?id=" + c + "&units=imperial&APPID=" + APIKey + "&cnt=5",
+        method: "GET"
+        })
+        .then(function(response) {
+
+            var date = response.list[0].dt_txt;
+            //date.toString();
+            date.replace(/-/g, "/");
+            //console.log(response);
+            console.log(date);
+            $(".date").text(date);
+    });
+}
